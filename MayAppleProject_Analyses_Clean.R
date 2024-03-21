79a0743786fd2ec35309a2e28dff3a1cf1f0cfc8
Melissa Burt <melissa.ann.burt@gmail.com>
2024-03-21 14:45:44 -0400

    Added script so that we can start cleaning up the code.

---
+++ MayAppleProject_Analyses_Clean.R
@@ -0,0 +1,24 @@
+#Mayapple Warming Experiment
+
+#People: Lee Matthew and Melissa Burt
+
+
+#Objective: Add an objective for this code here. I usually describe the project
+#including project aims. 
+
+
+
+#Load these packages:
+
+
+
+
+#Load Datasets - give instructions here for what dataset is used in the code. 
+
+
+
+
+##############################################################################
+##############################################################################
+##############################################################################
+##############################################################################
