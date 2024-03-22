e87a0da13025fdf188b42007a3766f0e34f88a57
lee matthew <150720010+leematthewlab@users.noreply.github.com>
2024-03-22 15:13:21 -0400

    Update MayAppleProject_Analyses_Clean.R

--- MayAppleProject_Analyses_Clean.R
+++ MayAppleProject_Analyses_Clean.R
@@ -1,6 +1,6 @@
 #Mayapple Warming Experiment
-
-#People: Lee Matthew and Melissa Burt
+#started: 10/30/2023
+#People: Lee Matthew (leematthew@vt.edu) and Melissa Burt
 
 
 #Objective: To find any significant differences in pollinator count, average temperature, 
@@ -17,15 +17,105 @@ library(ggplot2)
 
 #Load Datasets - give instructions here for what dataset is used in the code. 
 
+#most recent, relevant "dataset.3.4.2024"
 
-
-#most recent to least recent 
+#note: code was put here most recent to least recent 
+#-> this is because the two biggest dataset had to be redone becuase of missing data so most relevant code is most recent
 ##############################################################################
 ##############################################################################
 ##############################################################################
 ##############################################################################
 
 
+
+
+#11/29/2023
+#we trying to get those p values today ! >:)
+pairwise_t_test_temp_warm_vs_ambient <- pairwise.t.test(merged_data_temp_with_treatment$`Temperature (C)`, merged_data_temp_with_treatment$Treatment, data = merged_data_temp_with_treatment)
+print(pairwise_t_test_temp_warm_vs_ambient)
+#number give : 0.012 ! yippee?? it says P value adjustment method:holm
+#this was for the temperature dataset that was missing data
+#thought i would keep it in though
+
+#12/1/2023 - temperature with treatments
+#re-merge temperature_full with treatments, was edited in original data to contain Chamber_ID as well as Ibutton_ID
+#also when went back saw that first data set and missing data because of transfer so added that also
+
+str(MayappleData_Temperature_Full)
+na.omit(MayappleData_Temperature_Full)
+merged_data_temp_with_treatment_full <- merge(MayappleData_ChamberTreatments_Ibutton_ID_good, MayappleData_Temperature_Full, by = "Ibutton_ID")
+str(merged_data_temp_with_treatment_full)
+View(merged_data_temp_with_treatment_full)
+merged_data_temp_with_treatment_full$Treatment <- as.factor(merged_data_temp_with_treatment_full$Treatment)
+str(merged_data_temp_with_treatment_full)
+#yippee! new full temperature set merged with treatment and made ambient/warm into factors. 
+#redo t.test
+pairwise_t_test_temp_warm_vs_ambient_full <- pairwise.t.test(merged_data_temp_with_treatment_full$`Temperature (C)`, merged_data_temp_with_treatment_full$Treatment, data = merged_data_temp_with_treatment_full)
+print(pairwise_t_test_temp_warm_vs_ambient_full)
+#p value 0.056, not significant, heartbreaking, holm p value adjustment
+
+
+#comparing time of ambient vs warm total video length
+str(merged_data_attributes_with_treatment)
+merged_data_attributes_with_treatment$Treatment <- as.factor(merged_data_attributes_with_treatment$Treatment)
+str(merged_data_attributes_with_treatment)
+na.omit(merged_data_attributes_with_treatment)
+str(merged_data_attributes_with_treatment)
+sum(merged_data_attributes_with_treatment$TotalBloomLength_Days)
+
+#12/04/2023 - this too was redone to edited observation dataset
+#divided pollinator observation with time, ibutton ids into factors, connected to attributes
+#12/06/2023 - this too was redone to edited observation dataset
+#1/29/2024 - skipped because was redone to edited observation dataset
+
+#2/12/2024 - "data breach" found - pretty sure it was observation data?
+#ibuttons as factors
+MayappleData_Fixed_Temp_2$Ibutton_ID = as.factor(MayappleData_Fixed_Temp_2$Ibutton_ID)
+str(MayappleData_Fixed_Temp_2)
+#Make chamber id from number into factor with 10 levels
+MayappleData_Fixed_Temp_2$Chamber_ID = as.factor(MayappleData_Fixed_Temp_2$Chamber_ID)
+str(MayappleData_Fixed_Temp_2)
+
+
+#2/16/24 - temperature figures
+merged_mayappledata_fixed_temp_2_averaged <- merged_mayappledata_fixed_temp_2_filter %>%
+  group_by(Date, Treatment) %>%
+  summarise(Avg_Temperature = mean(`Temperature (C)`))
+
+#line graph temperature of treatments
+plot <- ggplot(merged_mayappledata_fixed_temp_2_averaged, aes(x = Date, y = Avg_Temperature, color = Treatment)) +
+  geom_line(size = 1) +
+  geom_text(aes(label = sprintf("%.1f", Avg_Temperature)), 
+            hjust = 0, vjust = 1, position = position_dodge(0.5), size = 2) + 
+  labs(title = "Average Ambient and Warm Temperatures Over Time",
+       x = "Date",
+       y = "Average Temperature (C)",
+       color = "Treatment") +
+  scale_x_date(date_labels = "%Y-%m-%d", date_breaks = "1 week") + 
+  scale_color_manual(values = c("#4CAF50", "#008000")) + 
+  theme_light() +
+  theme(plot.background = element_rect(fill = "#F5F5DC"))
+print(plot)
+
+average_temp_weekly <- merged_mayappledata_fixed_temp_2_filter %>%
+  group_by(Treatment, week = lubridate::week(Date)) %>%
+  summarise(Avg_Temperature = mean(`Temperature (C)`))
+
+#temperature graph but no line, just the data point dots
+plot2 <- ggplot(average_temp_weekly, aes(x = as.factor(week), y = Avg_Temperature, color = Treatment)) +
+  geom_line(size = 1.5) +
+  geom_point() +  # Add points for each data point
+  labs(title = "Average Ambient and Warm Temperatures Each Week",
+       x = "Week",
+       y = "Average Temperature (C)",
+       color = "Treatment") +
+  scale_color_manual(values = c("#4CAF50", "#008000")) +  # Set line colors
+  theme_minimal()
+
+print(plot2)
+
+#2/16/24 - tests gave weird results, no use
+#2/23/2024 - graphs no longer useful
 #2/26/2024 cont
 
 MayappleData_VisitorObservations <- MayappleData_VisitorObservations %>%
@@ -105,7 +195,7 @@ print(t_test_observation_count_divided_by_time_6)
 #results ata:  ambient_count_divided_by_time_6 and warm_count_divided_by_time_6
 #t = 8.3272, df = 111.35, p-value = 2.366e-13
 
-#plots
+#boxplots
 str(MayappleData_VisitorObservations_with_time_treatment_6)
 #rate of pollinator count in treatments
 plot6 <- ggplot(MayappleData_VisitorObservations_with_time_treatment_6, aes(x = Treatment, y = count_divided_by_time, fill = Treatment)) +
@@ -149,3 +239,4 @@ plot20 <- ggplot(seed_data, aes(x = Treatment, y = Count, fill = Treatment)) +
   scale_fill_manual(values = c("#4CAF50", "#008000", "#FF5733"))
 
 print(plot20)
+#fin: 03/17/2024
